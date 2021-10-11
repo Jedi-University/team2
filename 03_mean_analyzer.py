1811e0392139513551955771e2d0d387e7a68eb3
Denis Suvolokin <suvolokindn@yandex.ru>
2021-10-12 02:04:52 +0400

    change in_out to csv

--- 03_mean_analyzer.py
+++ 03_mean_analyzer.py
@@ -1,6 +1,13 @@
 import os
-import json
+import csv
 
+def digitizer(data):
+    data_width=len(data[0])
+    for i in data:
+      for j in range(data_width):
+        i[j]= (int(i[j]) if ("." not in i[j]) else float(i[j]))
+        j=+1
+    return data
 
 def my_mean(data):  # incoming_array
     acc = 0
@@ -21,13 +28,22 @@ directory = os.fsencode(dir_str)
 new_file = "advanced_ohlc.txt"
 new_file_path = dir_str + "/" + new_file
 
+try:
+    os.remove(new_file_path)
+except OSError:
+    pass
+
 
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     path = dir_str + "/" + filename
     if filename.startswith("_"):
-        with open(path, "r") as f:
-            data = f.read()
-            data = json.loads(data)  # string to list
-        with open(new_file_path, "a+") as f:
-            f.write(str(my_filter(data)))
+        with open(path, "r",newline='') as f:
+            # data = f.read()
+            reader=csv.reader(f)
+            data = list(reader)  # string to list
+            data = digitizer(data)
+        with open(new_file_path, "a+",newline='') as f:
+            # f.write(str(my_filter(data)))
+            csv_out=csv.writer(f)
+            csv_out.writerows(data[index] for index in range(len(data)))

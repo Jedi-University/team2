b54bc98ea3798f19f748c4cca4b374e50081cc8b
Denis Suvolokin <suvolokindn@yandex.ru>
2021-10-12 23:17:05 +0400

    hakerrank homework

---
+++ dsuvolokin_hw/hakerrank_hw.py
@@ -0,0 +1,33 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    # n = int(raw_input().strip())
+    # w='Weird';nw='Not Weird'
+    # my_dict={n%2!=0 : w ,( n%2==0 and n in range(2,6)) : nw, (n%2==0 and n in range(6,21)) : w, n>20 : nw}
+    # print(my_dict[True])
+    ########
+    # a=3;b=5;nl='\n'
+    # print(a+b,a-b,a*b,sep=nl)
+    ########
+    # a=4;b=3;nl='\n'
+    # print(a//b,a/b,sep=nl)
+    ########
+    # n=3
+    # for i in range(n):
+    # 	print(pow(i,2))
+    ########
+    # n=1900
+    # leap = (n%4==0) and ((n%100!=0) or (n%400==0))
+    # print(leap)
+    ########
+    # n=5; acc=0
+    # for i in range(n):
+    #   acc=acc*10+(i+1)
+    # print(acc)

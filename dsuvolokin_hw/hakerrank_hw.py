8571d7b8d6e8999f295c4d01957230f13ca6cd21
Denis Suvolokin <suvolokindn@yandex.ru>
2021-10-14 00:56:43 +0400

    add extra tasks

--- dsuvolokin_hw/hakerrank_hw.py
+++ dsuvolokin_hw/hakerrank_hw.py
@@ -31,3 +31,21 @@ if __name__ == '__main__':
     # for i in range(n):
     #   acc=acc*10+(i+1)
     # print(acc)
+    ########
+    # def func(*args):
+    #     return lambda l: list(l+every for every in list(args))
+    # f = func("a", "b", "c")
+    # print(f("x"))
+    ########
+    ##код который превратит его в простой лист 
+    # l = [[1,2,3,4,5,6], [0,0,0,0], ["a", "b", "c"]]
+    #
+    # l_len=len(l)
+    # for i in range(l_len):
+    #     for j in l[i]:
+    #         l.append(j)
+    #
+    # for i in range(l_len):
+    #     del l[0]
+    #
+    # print(l)

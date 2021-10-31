19323bfc84d6ebb925a746acc8685e9a33a91c5c
romazanovma <68828332+romazanovma@users.noreply.github.com>
2021-10-31 16:35:27 +0400

    Create github.auth

--- mromazanov/github_top/toprep.py
+++ mromazanov/github_top/toprep.py
@@ -11,7 +11,7 @@ import db
 
 def auth():
     with open('github.auth', 'r') as auth:
-        info = auth.read().strip().split(',')
+        info = auth.readline().strip().split(',')
     return (info[0], info[1])
 
 

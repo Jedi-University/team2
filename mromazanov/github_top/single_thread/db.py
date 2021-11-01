22c3ced2225ae4264ac94d39977da0ffe9947fae
Maxim Romazanov <romazanovma@gmail.com>
2021-11-01 20:42:49 +0400

    added multithreading(githubapi.py, commented)

---
+++ mromazanov/github_top/single_thread/db.py
@@ -0,0 +1,55 @@
+from sqlalchemy import create_engine, MetaData, Table, Integer, String, Column, DateTime, ForeignKey, Numeric, engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import mapper, Session
+
+from tkinter import *
+from tkinter import messagebox, scrolledtext
+
+import githubapi
+
+base = declarative_base()
+
+
+class Top(base):
+    __tablename__ = 'Top'
+    id = Column(Integer, primary_key=True)
+    org_name = Column(String(100), nullable=False)
+    repo_name = Column(String(100), nullable=False)
+    stars_count = Column(Integer())
+    
+    def show(self,sep):
+        return (self.id, sep, self.org_name, sep, self.repo_name, sep, self.stars_count, '\n')
+
+
+def create_db(auth):
+    global repo_stars, repo_names
+    base.metadata.drop_all(engine)
+    base.metadata.create_all(engine)
+
+    top_repos_dict = githubapi.create_top(auth)
+    top_repos = []
+    for repo in top_repos_dict:
+        top_repos.append(Top(id = repo['id'], org_name = repo['org_name'], repo_name = repo['repo_name'], stars_count = repo['stars_count']))
+    session.add_all(top_repos)
+    session.commit()
+    messagebox.showinfo('', 'Успешно')
+
+
+def show_db():
+    show = session.query(Top).all()
+    show_win = Tk()
+    show_win.title('Top')
+    #show_win.geometry("1024x768")
+    txt = scrolledtext.ScrolledText(show_win, width=120, height=45)
+    txt.delete(1.0, END)
+    txt.insert(INSERT, 'id\t\torg_name\t\trepo_name\t\tstars_count\n')
+    for item in show:
+        txt.insert(INSERT, f'{item.id}\t\t{item.org_name}\t\t{item.repo_name}\t\t{item.stars_count}\n')
+    txt.grid(column=0, row=1)
+    ok_button = Button(show_win, text="ok", command=show_win.destroy)
+    ok_button.grid(column=0, row=0)
+
+
+engine = create_engine('sqlite:///reptop.db')
+engine.connect()
+session = Session(bind=engine)
--- mromazanov/github_top/single_thread/githubapi.py
+++ mromazanov/github_top/single_thread/githubapi.py
@@ -1,21 +1,13 @@
-from multiprocessing import queues
-from multiprocessing.queues import Queue
 from tkinter.font import names
 import requests
 import heapq
-import multiprocessing
-from queue import Queue
-import threading
-from threading import Thread
 
 
-def get_repos(name, auth, results):
-    thread_name = threading.current_thread().name
-    print(f'Старт {thread_name}')
+def get_repos(name, auth):
+    global repo_stars, repo_names
     response_API = [1]
     page = 1
     repos = []
-    print(name)
     while True:
         response = requests.get(f'https://api.github.com/orgs/{name}/repos?per_page=100&page={page}', auth=auth)
         response_API = response.json()
@@ -24,13 +16,15 @@ def get_repos(name, auth, results):
                 id = repo['id']
                 name = repo['full_name']
                 stars = repo['stargazers_count']
-                results.put([id,name,stars])
-                print(id)
+                repo_stars[id] = stars
+                repo_names[id] = name
             except TypeError:
                 break
         page += 1
         if (response.status_code == 404) or (response_API == []):
             break
+        repos.extend(response_API)
+    return repos
 
 
 def get_names(orgs):
@@ -52,28 +46,12 @@ def get_orgs(pages, auth):
 
 
 def create_top(auth):
-    global last_id, orgs, repo_names, repo_stars
+    global last_id, orgs, repo_names, repo_stars, repos
     for org in get_orgs(2, auth):
         orgs.extend(org)
     names = get_names(orgs)
-    #names = ['Jedi-University']
-
-    #queue = Queue()
-    #for name in names:
-    #    queue.put(name)
-    #while queue.empty() is False:
-    #    threads = []
-    #    for i in range(8):
-    #        threads.append(Thread(target = get_repos, args = (queue.get(), auth, results)))
-    #        threads[i].start()
-    #    for i in range(8):
-    #        threads[i].join()
-
-    while results.empty() is False:
-        elem = results.get()
-        print(elem)
-        repo_names[elem[0]] = elem[1]
-        repo_stars[elem[0]] = elem[2]
+    for name in names:
+       repos.extend(get_repos(name, auth))
     repo_stars = heapq.nlargest(20, repo_stars.items(), key=lambda i: i[1])
 
     top_repos = []
@@ -84,6 +62,7 @@ def create_top(auth):
     orgs = []
     repo_stars = {}
     repo_names = {}
+    repos = []
     return top_repos
 
 
@@ -91,4 +70,4 @@ last_id = 0
 orgs = []
 repo_stars = {}
 repo_names = {}
-results = multiprocessing.Queue()
+repos = []

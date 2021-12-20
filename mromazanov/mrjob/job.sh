1bce6f3417a7fa50df90a27889e4a5a33661e9bb
Maxim Romazanov <romazanovma@gmail.com>
2021-12-20 15:37:44 +0400

    updated mrjob

--- mromazanov/mrjob/job.sh
+++ mromazanov/mrjob/job.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-time python3 jobs/job01.py $1 >output/temp01
-time python3 jobs/job02.py $1 >output/temp02
-time python3 jobs/job1.py output/temp02 >output/output1
-time python3 jobs/job2.py output/temp02 >output/output2
-time python3 jobs/job3.py output/temp01 >output/output3
+mkdir output
+time python3 jobs/preprocessing1.py $1 >output/temp01
+time python3 jobs/preprocessing2.py $1 >output/temp02
+time python3 jobs/aggr_ip_clicks.py output/temp02 >output/grouped.txt
+time python3 jobs/aggr_loc_clicks.py output/temp02 >output/location.txt
+time python3 jobs/time_clicks.py output/temp01 >output/time_location.txt
+rm output/temp*
